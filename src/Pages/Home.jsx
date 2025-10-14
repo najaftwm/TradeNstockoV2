f6ef7df31e198cead9ef0b0b4b879b107afb2c6c
Aaditi <najaf@twmresearchalert.com>
2025-10-14 09:10:17 +0530

    Added floating whatsapp button

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,11 +5,13 @@ import AboutUs from '../Component/AboutUs'
 import ReviewsSection from '../Component/ReviewsSection'
 import LoginPage from '../Component/LoginPage'
 import Features from '../Component/Features'
+import FloatingWhatsAppButton from '../Component/FloatingWhatsAppButton'
 
 const Home = () => {
   return (
     <div>
         <HeroSection/>
+        <FloatingWhatsAppButton/>
         <ScrollingText/>
         <Features/>
         <LoginPage/>
