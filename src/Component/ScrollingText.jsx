d04bd2328437cf25675be8061543a0dd0d4617f1
Aaditi <najaf@twmresearchalert.com>
2025-10-15 12:09:22 +0530

    fixed the jump transition of scrolling text

--- src/Component/ScrollingText.jsx
+++ src/Component/ScrollingText.jsx
@@ -1,26 +1,25 @@
 import React from "react";
 
 export default function ScrollingText() {
+  const scrollText = (
+    <>
+      <span> Trade Smart • </span>
+      <span> Lowest Brokerage • </span>
+      <span> Fast Withdrawals • </span>
+      <span> 24/7 Support • </span>
+    </>
+  );
+
   return (
     <section className="relative flex items-center justify-center h-[180px] bg-[#EDE8D0] -mt-6 overflow-hidden">
       {/* Divider line above section */}
       <div className="absolute top-6 left-0 w-full h-[2px] bg-black/20"></div>
 
-      <div className="scrolling-wrapper whitespace-nowrap">
-        {/* First scroll content */}
+      <div className="scrolling-wrapper">
         <div className="scrolling-content text-[60px] md:text-[70px] font-extrabold">
-          <span> Trade Smart • </span>
-          <span> Lowest Brokerage • </span>
-          <span> Fast Withdrawals • </span>
-          <span> 24/7 Support • </span>
-        </div>
-
-        {/* Duplicate for seamless scroll */}
-        <div className="scrolling-content text-[60px] md:text-[70px] font-extrabold">
-          <span> Trade Smart • </span>
-          <span> Lowest Brokerage • </span>
-          <span> Fast Withdrawals • </span>
-          <span> 24/7 Support • </span>
+          {scrollText}
+          {scrollText}
+          {scrollText}
         </div>
       </div>
     </section>
